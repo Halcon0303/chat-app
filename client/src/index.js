0ea152c0d01195cd17e343566e83c5f3a7ef7c2b
Halcon0303 <ahalcon33@gmail.com>
2024-01-01 22:06:50 +0700

    Setup 25% 1/1/2024

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Context
+import SettingsContext from "./contexts/SettingsContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <SettingsContext>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SettingsContext>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
---
+++ client/src/pages/dashboard/GeneneralApp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GeneneralApp = () => {
+  return <div>App</div>;
+};
+
+export default GeneneralApp;
