29496dbd74b7ad5cda33e6547a582a15dc66d444
Halcon0303 <ahalcon33@gmail.com>
2024-01-07 00:04:23 +0700

    Done App bar and dark mode
    Done App bar and dark mode
    
    Co-Authored-By: Trang <69392580+chan1010@users.noreply.github.com>
    Co-Authored-By: Minh Tan <148736142+minhtan7899@users.noreply.github.com>

--- client/src/components/AntSwitch.js
+++ client/src/components/AntSwitch.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Switch } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
--- client/src/layouts/dashboard/index.js
+++ client/src/layouts/dashboard/index.js
@@ -1,10 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { Box, Stack, IconButton, Divider, Avatar, Switch } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { Gear } from "phosphor-react";
+import { faker } from "@faker-js/faker";
+import { Nav_Buttons } from "../../data";
+import Logo from "../../assets/Images/logo.ico";
+import AntSwitch from "../../components/AntSwitch";
+import useSettings from "./../../hooks/useSettings";
 
 const DashboardLayut = () => {
+  const theme = useTheme();
+  const [selected, setSelected] = useState(0);
+  const { onToggleMode } = useSettings();
+
   return (
     <>
-      <div>DashboardLayut</div>
+      <Box
+        p={2}
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          height: "100vh",
+          width: 100,
+        }}
+      >
+        <Stack
+          direction={"column"}
+          sx={{ height: "100%" }}
+          alignItems={"center"}
+          spacing={3}
+          justifyContent={"space-between"}
+        >
+          <Stack alignItems={"center"} spacing={4}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 1.5,
+              }}
+            >
+              <img src={Logo} alt="logo" />
+            </Box>
+            {/* Icon Contact */}
+            <Stack
+              sx={{ width: "max-content" }}
+              spacing={3}
+              direction={"column"}
+              alignItems={"center"}
+            >
+              {Nav_Buttons.map((val) => {
+                return val.index === selected ? (
+                  <Box
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                    key={val.index}
+                  >
+                    <IconButton sx={{ width: "max-content", color: "#ffffff" }}>
+                      {val.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    sx={{
+                      width: "max-content",
+                      color:
+                        theme.palette.mode === "light"
+                          ? "#000000"
+                          : theme.palette.text.primary,
+                    }}
+                    key={val.index}
+                    onClick={() => setSelected(val.index)}
+                  >
+                    {val.icon}
+                  </IconButton>
+                );
+              })}
+              <Divider sx={{ width: "48px" }} />
+              {/* Icon Settings */}
+              {selected === 3 ? (
+                <Box
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton sx={{ width: "max-content", color: "#ffffff" }}>
+                    <Gear />
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => setSelected(3)}
+                  sx={{
+                    width: "max-content",
+                    color:
+                      theme.palette.mode === "light"
+                        ? "#000000"
+                        : theme.palette.text.primary,
+                  }}
+                >
+                  <Gear />
+                </IconButton>
+              )}
+            </Stack>
+          </Stack>
+          {/* Avatar */}
+          <Stack spacing={4}>
+            <AntSwitch onChange={() => onToggleMode()} defaultChecked />
+            <Avatar src={faker.image.avatar()} />
+          </Stack>
+        </Stack>
+      </Box>
       <Outlet />
     </>
   );
